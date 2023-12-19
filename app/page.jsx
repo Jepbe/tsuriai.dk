fe05258338c6acf8fdb6710275788d702b3b6804
Jeppe Bøgeskov <67188645+Jepbe@users.noreply.github.com>
2023-12-19 16:15:12 +0100

    Initial

--- app/page.jsx
+++ app/page.jsx
@@ -4,16 +4,19 @@ import Navbar from './ui/navbar'
 export default function Home() {
   return ( 
 <>
-    <Navbar 
+    {/* <Navbar 
       text1="navbar1"
       text2="navbar2"
       text3="navbar3"  
       text4="navbar4"  
       text5="navbar5"  
-    />
+    /> */}
 
-    <main>
-      <h1>Hello</h1>
+    <main className='w-screen h-screen grid place-items-center'>
+      <div>
+        <h1>Tsuriai Ryu Ju Jitsu</h1>
+        <h3>Slagelse Selvforsvarsklub</h3>
+      </div>
 
     </main>
 </>
