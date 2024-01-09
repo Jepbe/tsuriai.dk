8fe46b0ebf3f934d5d408f43ae12b16adc2be2a5
Jeppe Bøgeskov <67188645+Jepbe@users.noreply.github.com>
2024-01-09 17:03:59 +0100

    production build test

--- app/ui/dashboard/sidenav.jsx
+++ app/ui/dashboard/sidenav.jsx
@@ -9,6 +9,8 @@ import { useState } from 'react';
 
 export default function SideNav() {
   const [activeButton, setActiveButton] = useState('');
+  const [navbar, setNavbar] = useState( false );
+
  
   const handleButtonClick = (buttonName) => {
     setActiveButton(buttonName);
@@ -17,13 +19,23 @@ export default function SideNav() {
   
   return(
     <>
-            <button className="sm:hidden inline-flex items-center justify-center text-black rounded-lg p-6">
-                <svg className="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <button onClick={() => setNavbar(!navbar)} className="sm:hidden absolute right-4 top-4 z-10 items-center justify-center text-black rounded-lg">
+                {navbar ?
+                <span className='relative left-[2px] bottom-1'>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                  </svg>
+                </span> 
+                :
+                <span className='relative right-5'>
+                  <svg className="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-                </svg>
+                  </svg>
+                </span>
+                }
             </button>
 
-          <div className='h-screen w-[288px] bg-white hidden sm:flex flex-col p-4 drop-shadow font-medium'>
+          <div className={`h-screen w-screen sm:w-[288px] bg-white sm:flex flex-col p-4 drop-shadow font-medium ${navbar ? 'flex' :'hidden'}`}>
             <div className='w-full mb-10 py-2'>
               <Image src={tsuriaiLogo} alt='Tsuriai Logo' width={75} height={75}></Image>
             </div>
@@ -52,7 +64,7 @@ export default function SideNav() {
               </div>
             </Link>
                                                                 {/* SKIFT FØR COMMIT  */}
-            <Link onClick={ () => handleButtonClick('button4')} href="http://localhost:3000/dashboard/green">
+            <Link onClick={ () => handleButtonClick('button4')} href="https://tsuriai-dk.vercel.app//dashboard/green">
               <div className={`${'px-2 py-[10px] hover:bg-[#f3f3f3] hover:cursor-pointer rounded mt-1'} 
                 ${activeButton === "button4" ? 'bg-[#f3f3f3]' : ''}`}>
                 <span className='flex flex-row gap-2 items-center'><i>🟩</i>Grønt Pensum</span>
--- app/ui/navbar/navbar-server.jsx
+++ app/ui/navbar/navbar-server.jsx
@@ -6,13 +6,13 @@ export default async function NavbarServer() {
     const session = await getServerSession(options);
 
     return (
-        <ul className="flex flex-col text-[16px] md:flex-row gap-9 items-center md:hidden">
+        <>
             {session ?<li className="drop-shadow"> <Link href="/member">member</Link></li> : <li className="hidden"></li>}
             {session ?<li className="drop-shadow"> <Link href="/dashboard">dashboard</Link></li> : <li className="hidden"></li>}
             <li className="drop-shaddow">Hold</li>
             <li className="drop-shaddow"><Link href="./hvemervi">Hvem er vi?</Link></li>
             <li className="drop-shaddow"><Link href="../../#kontakt">Kontakt</Link></li>    
             {session ? <button className="bg-blue-500 rounded text-white px-3 py-1"><Link href="/api/auth/signout?callbackUrl=/">Log ud</Link></button> : <button className="bg-blue-500 rounded text-white px-3 py-1"><Link href="/api/auth/signin/github">log in</Link></button> }
-        </ul>
+        </>
     );
 }
